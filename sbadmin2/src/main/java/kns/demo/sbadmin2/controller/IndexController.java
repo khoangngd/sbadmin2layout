942a2bfa5ed26966458067589eac2d90aad71329
dangkhoanguyen.shinhan <dangkhoanguyen.shinhan@gmail.com>
2022-04-06 13:25:04 +0700

    add internationlization

--- sbadmin2/src/main/java/kns/demo/sbadmin2/controller/IndexController.java
+++ sbadmin2/src/main/java/kns/demo/sbadmin2/controller/IndexController.java
@@ -1,12 +1,19 @@
-package kns.demo.sbadmin2.controllers;
+package kns.demo.sbadmin2.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private MessageSource messageSource;
+
     @RequestMapping(value = {"/","/home","/index"}, method = RequestMethod.GET)
     public String indexPage(){
         return "index";
