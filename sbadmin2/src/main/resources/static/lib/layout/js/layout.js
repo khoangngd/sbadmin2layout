5df4d1a35e869cd228ca938994b5418fa475f578
dangkhoanguyen.shinhan <dangkhoanguyen.shinhan@gmail.com>
2022-04-08 08:30:47 +0700

    pacejs, simple-notify

--- sbadmin2/src/main/resources/static/lib/layout/js/layout.js
+++ sbadmin2/src/main/resources/static/lib/layout/js/layout.js
@@ -7,6 +7,35 @@
 
 })();
 
+function getContextPath() {
+    return window.location.pathname.substring(0, window.location.pathname.indexOf("/",2));
+}
+var contextPath = getContextPath();
+
+function alertExport(value){
+    if(value){
+        new Notify ({
+            title: 'Notification',
+            text: 'Successfully',
+            status: 'success',
+            autoclose: true,
+            autotimeout: 3000,
+            position: 'right bottom'
+        });
+    }else{
+        new Notify ({
+            title: 'Notification',
+            text: 'Error',
+            status: 'error',
+            autoclose: true,
+            autotimeout: 3000,
+            position: 'right bottom'
+        });
+    }
+}
+
+$(document).ajaxStart(function() { Pace.restart(); });
+
 // angular.module('app').controller('page.layout', [
 //     '$scope', '$timeout',
 //     function ($scope, $timeout) {
